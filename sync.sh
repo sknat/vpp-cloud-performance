f4d3adbac2b54af18898c61ac3f38b44d9bf3b0d
Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
2020-02-13 16:01:46 +0100

    Add ssh commands
    Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

--- sync.sh
+++ sync.sh
@@ -2,6 +2,15 @@
 
 source $( dirname "${BASH_SOURCE[0]}" )/shared.sh
 
+_ssh_cmd ()
+{
+  ssh \
+    -i $LOCAL_IDENTITY_FILE \
+    -o ProxyCommand="ssh -W %h:%p $USERNAME@$BASTION_IP -i $LOCAL_IDENTITY_FILE" \
+    -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" \
+    $USERNAME@$1 ${@:2}
+}
+
 sync ()
 {
   rsync -avz                      \
@@ -14,12 +23,7 @@ sync ()
     $SCRIPTDIR/                   \
     $LOCAL_IDENTITY_FILE          \
     $USERNAME@$VM2_MANAGEMENT_IP:~/test/
-  ssh \
-    -i $LOCAL_IDENTITY_FILE \
-    -o ProxyCommand="ssh -W %h:%p $USERNAME@$BASTION_IP -i $LOCAL_IDENTITY_FILE" \
-    -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" \
-    $USERNAME@$VM2_MANAGEMENT_IP \
-    -t '~'"/test/sync.sh local-sync $1"
+  _ssh_cmd $VM2_MANAGEMENT_IP -t '~'"/test/sync.sh local-sync $1"
 }
 
 _local_rsync ()
@@ -42,16 +46,38 @@ local_sync ()
   _local_rsync $ROUTER2_MANAGEMENT_IP
 }
 
+print_usage ()
+{
+  echo "Usage:"
+  echo "sync.sh conf_file sync              - sync to vms"
+  echo "sync.sh conf_file [vm1|vm2|sw1|sw2] - get a shell in an instance"
+}
+
 sync_cli ()
 {
   # TESTING
   if [[ "$1" = "" ]]; then
-    echo "Please provide a config file"
+    print_usage
   elif [[ "$1" = "local-sync" ]]; then
     local_sync ${@:2}
+    exit 0
   else
     source $1
-    sync ${1##*/}
+    FNAME=$1
+  fi
+  shift
+  if [[ "$1" = "sync" ]]; then
+    sync ${FNAME##*/}
+  elif [[ "$1" = "vm1" ]]; then
+    _ssh_cmd $VM1_MANAGEMENT_IP
+  elif [[ "$1" = "vm2" ]]; then
+    _ssh_cmd $VM2_MANAGEMENT_IP
+  elif [[ "$1" = "sw1" ]]; then
+    _ssh_cmd $ROUTER_MANAGEMENT_IP
+  elif [[ "$1" = "sw2" ]]; then
+    _ssh_cmd $ROUTER2_MANAGEMENT_IP
+  else
+    print_usage
   fi
 }
 
